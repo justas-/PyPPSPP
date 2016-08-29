b81a1a40ae989ce3ceb3ac9934e7010dcc91419f
justas- <justas.poderys@gmail.com>
2016-08-29 09:41:45 +0200

    Add command line arguments parsing

--- PyPPSPP/PyPPSPP.py
+++ PyPPSPP/PyPPSPP.py
@@ -3,6 +3,8 @@ import asyncio
 import os
 import socket
 import binascii
+import sys
+import getopt
 
 from PeerProtocol import PeerProtocol
 from TrackerClientProtocol import TrackerClientProtocol
@@ -12,70 +14,95 @@ from SimpleTracker import SimpleTracker
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 logging.info ("PyPPSPP starting")
 
-# Start minimalistic event loop
-loop = asyncio.get_event_loop()
-loop.set_debug(True)
+def main(argv):
+    # TODO: Move whatever possible to tracker
+    # Leave here just download directory
+    trackerip = ''      
+    filename = ''
+    swarmid = ''
+    filesize = 0
 
-tracker = SimpleTracker()
+    try:
+        opts, args = getopt.getopt(argv, "t:f:s:z", ["tracker=", "filename=", "swarmid=", "filesize="])
+    except getopt.GetoptError:
+        print("Error parsing command line arguments")
+        sys.exit(1)
 
-# Create connection to the tracker node
-# All this code should be hidden in the Tracekr and Swarm manager!
-tracker_listen = loop.create_connection(lambda: TrackerClientProtocol(tracker), '127.0.0.1', 6777)
-tracker_transport, traceker_server = loop.run_until_complete(tracker_listen)
+    if len(opts) == 0:
+        # Set the defaults
+        trackerip = '127.0.0.1'
+        filename = 'test10MB.bin'
+        swarmid = '87a5e6618b2af6f92854eb83e2664d09af7db138'
+        filesize = 10485788
+    else:
+        for opt, arg in opts:
+            if opt in ("-t", "--tracker"):
+                trackerip = arg
+            elif opt in ("-f", "--filename"):
+                filename = arg
+            elif opt in ("-s", "--swarmid"):
+                swarmid = arg
+            elif opt in ("-z", "--filesize"):
+                filesize = int(arg)
 
-# TODO - check if connected to the Tracekr!
-tracker.SetTrackerProtocol(traceker_server)
+    logging.info("PPSPP Parameters:\n\tTracker: {0};\n\tFilename: {1};\n\tFilesize: {2}B;\n\tSwarm: {3}"
+                 .format(trackerip, filename, filesize, swarmid))
 
-# Create an UDP server
-# Since we are using Tracker, we don't have to run on 6778 port!
-listen = loop.create_datagram_endpoint(PeerProtocol, local_addr=("0.0.0.0", 6778))
-transport, protocol = loop.run_until_complete(listen)
+    # Start minimalistic event loop
+    loop = asyncio.get_event_loop()
+    loop.set_debug(True)
 
-# At this point we have connection to the tracker and listening UDP socket
+    tracker = SimpleTracker()
 
-# Start the swarm
-# TODO: This is not the right place to do it...
+    # Create connection to the tracker node
+    # All this code should be hidden in the Tracekr and Swarm manager!
+    tracker_listen = loop.create_connection(lambda: TrackerClientProtocol(tracker), trackerip, 6777)
+    tracker_transport, traceker_server = loop.run_until_complete(tracker_listen)
 
-#swarm_id = binascii.unhexlify("82d3614b17dcac7624e58b2bee9bca1580a87b75")
-#swarm_filename = "C:\PyPPSPP\HelloWorld.txt"
-#swarm_file_size = 33
+    # TODO - check if connected to the Tracekr!
+    tracker.SetTrackerProtocol(traceker_server)
 
-swarm_id = binascii.unhexlify("87a5e6618b2af6f92854eb83e2664d09af7db138")
-swarm_filename = r"C:\PyPPSPP\test10MB.bin"
-swarm_file_size = 10485788
+    # Create an UDP server
+    # Since we are using Tracker, we don't have to run on 6778 port!
+    listen = loop.create_datagram_endpoint(PeerProtocol, local_addr=("0.0.0.0", 6778))
+    transport, protocol = loop.run_until_complete(listen)
 
-#swarm_id = binascii.unhexlify("87323eba55d370afcbfdf40ee4fd2b248bb98afb")
-#swarm_filename = r"C:\PyPPSPP\test1MB.bin"
-#swarm_file_size = 1048536
+    # At this point we have connection to the tracker and listening UDP socket
 
-# Create the swarm
-protocol.init_swarm(swarm_id, swarm_filename, swarm_file_size)
+    # Start the swarm
+    # TODO: This is not the right place to do it...
 
-# Inform tracker about swarm ready to receive connections
-tracker.SetSwarm(protocol.swarm)
+    # Create the swarm
+    protocol.init_swarm(swarmid, filename, filesize)
 
-# Register with the tracker
-tracker.RegisterWithTracker(swarm_id)
+    # Inform tracker about swarm ready to receive connections
+    tracker.SetSwarm(protocol.swarm)
 
-# Schedule wakeups to catch Ctrl+C in Win32
-# This should be fixed in Python 3.5 
-# Ref: http://stackoverflow.com/questions/24774980/why-cant-i-catch-sigint-when-asyncio-event-loop-is-running
-if os.name == 'nt':
-    def wakeup():
-        # Call again later
+    # Register with the tracker
+    tracker.RegisterWithTracker(swarmid)
+
+    # Schedule wakeups to catch Ctrl+C in Win32
+    # This should be fixed in Python 3.5 
+    # Ref: http://stackoverflow.com/questions/24774980/why-cant-i-catch-sigint-when-asyncio-event-loop-is-running
+    if os.name == 'nt':
+        def wakeup():
+            # Call again later
+            loop.call_later(0.5, wakeup)
         loop.call_later(0.5, wakeup)
-    loop.call_later(0.5, wakeup)
 
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
 
-# Inform protocol to close
-protocol.CloseProtocol()
+    # Inform protocol to close
+    protocol.CloseProtocol()
 
-tracker.UnregisterWithTracker()
+    tracker.UnregisterWithTracker()
 
-# Clean-up
-transport.close()
-loop.close()
+    # Clean-up
+    transport.close()
+    loop.close()
+
+if __name__ == "__main__":
+    main(sys.argv[1:0])
